c1b4647783444545bb7a9b6366bf8633fc607da2
greggy <sudakovgrigory@gmail.com>
2023-12-18 18:12:25 +0700

    initial

---
+++ app/src/main/java/su/cus/anonce/MainActivity.kt
@@ -0,0 +1,26 @@
+package su.cus.anonce
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import su.cus.anonce.databinding.ActivityMainBinding
+import su.cus.anonce.ui.theme.AnonceTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
